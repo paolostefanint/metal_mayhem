76533d43acc2c25714b843efc189582c707b493f
sickOscar <oscarchine@gmail.com>
2023-08-31 14:33:05 +0200

    fix: add nginx

--- joypad/src/shared/constants.ts
+++ joypad/src/shared/constants.ts
@@ -1,7 +1,7 @@
 const url = new URL(window.location.href);
 const HOSTNAME = `${url.protocol}//${url.hostname}${url.port ? `:${url.port}` : ''}`;
 const HOSTNAME_NO_PROTOCOL = `${url.hostname}${url.port ? `:${url.port}` : ''}`;
-const AUTH_REDIRECT_URI = `${HOSTNAME}`;
+const AUTH_REDIRECT_URI = `${HOSTNAME}/j`;
 enum SLIDER_TYPE {
   MILITARY = "military",
   PRODUCTION = "production",
